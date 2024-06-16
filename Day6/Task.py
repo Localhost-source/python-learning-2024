02b79672a36ce813f5bc89090434698e348c3750
New Localhost <uzwalsharma745@gmail.com>
2024-06-16 19:16:20 +0545

    Adding Some Steps

--- Day6/Task.py
+++ Day6/Task.py
@@ -7,9 +7,3 @@ c = f"{a} {b}"
 print(c)
 
 
-# Before python version 3.6 f-strings were not introduces
-language1 = "Python"
-language2 = "JS"
-message = "Hello I am learning {} and I am also learning {}".format(language1, language2)
-message = "Hello I am learning {} and I am also learning".format(language2, language1)
-print(message)
